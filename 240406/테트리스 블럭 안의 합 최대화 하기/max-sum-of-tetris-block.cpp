339cc1b99c48929986af9d324f5b54f48759ecc8
Seongguk Park <126008521+SeonggukPark@users.noreply.github.com>
2024-04-06 21:47:49 +0900

    Title: 테트리스 블럭 안의 합 최대화 하기, Time: 29ms, Memory: 0MB, Status: Passed - CodeTree

--- 240406/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.cpp
+++ 240406/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.cpp
@@ -55,8 +55,8 @@ void run(){
         }
     }
 
-    for (int i = 1; i <= n; ++i) {
-        for (int j = 1; j <= m; ++j) {
+    for (int i = 1; i < n; ++i) {
+        for (int j = 1; j < m; ++j) {
             int t = grid[i][j];
             int mini = 1001;
 
