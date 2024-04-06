37c35ad5206c61450cc20120347dbbb79f84a072
Seongguk Park <126008521+SeonggukPark@users.noreply.github.com>
2024-04-06 22:09:09 +0900

    Title: 외주 수익 최대화하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240406/외주 수익 최대화하기/max-of-outsourcing-profit.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+constexpr size_t MAX_N = 16;
+int arr[MAX_N];
+
+int main() {
+    int n, t, p;
+    cin >> n;
+
+    memset(arr, 0, sizeof(arr));
+
+    for(int i = 0; i < n; i++){
+      cin >> t >> p;
+      arr[i + t] = max(arr[i + t], arr[i] + p);
+    }
+
+    cout << arr[n];
+    return 0;
+}
