8ab6db364f577b07cacc7691b65f6b376b965698
Seongguk Park <126008521+SeonggukPark@users.noreply.github.com>
2024-04-08 02:17:08 +0900

    Title: 보도블럭, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240408/보도블럭/crosswalk.cpp
+++ 240408/보도블럭/crosswalk.cpp
@@ -4,6 +4,7 @@ using namespace std;
 constexpr size_t MAX_N = 100;
 int n, l, rst;
 int grid[MAX_N][MAX_N];
+int cp_grid[MAX_N][MAX_N];
 bool visited[MAX_N][MAX_N];
 
 void init(){
@@ -72,54 +73,19 @@ void run(){
     }
 
     memset(visited, false, sizeof(visited));
+}
 
-    // 2. 열 scan
-    for (int i = 0; i < n; ++i){
-        int dir, idx, pivot, prev, next = 0;
-        bool flag = false;
-
-        while(next < n - 1){
-            prev = next, next++;
-
-            if(grid[prev][i] == grid[next][i]) continue;
-            if(abs(grid[prev][i] - grid[next][i]) != 1) {
-                flag = true;
-                break;
-            }
-
-            if(grid[prev][i] > grid[next][i]){
-                dir = 1;
-                idx = next;
-                pivot = prev;
-            }
-
-            else{
-                dir = -1;
-                idx = prev;
-                pivot = next;
-            }
-
-
-            for (int k = 0; k < l; ++k) {
-                if(idx < 0 || idx >= n || visited[idx][i] || grid[pivot][i] - grid[idx][i] != 1){
-                    flag = true;
-                    break;
-                }
-
-                visited[idx][i] = true;
-                idx += dir;
-            }
-
-            if(flag){
-                break;
-            }
+void rotate_grid(){
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cp_grid[i][j] = grid[i][j];
         }
+    }
 
-
-        if(!flag){
-            rst++;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            grid[j][n - 1 - i] = cp_grid[i][j];
         }
-
     }
 }
 
@@ -127,6 +93,9 @@ void run(){
 int main() {
     init();
     input();
+
+    run();
+    rotate_grid();
     run();
 
     cout << rst;
