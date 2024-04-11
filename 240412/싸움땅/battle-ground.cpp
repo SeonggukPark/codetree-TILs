c00973cb907016fbcdf59acd3f22959ce183815f
Seongguk Park <126008521+SeonggukPark@users.noreply.github.com>
2024-04-12 05:07:53 +0900

    Title: 싸움땅, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240412/싸움땅/battle-ground.cpp
+++ 240412/싸움땅/battle-ground.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <cstring>
 #include <queue>
-#define pii pair<int, int>
 using namespace std;
 constexpr size_t MAX_N = 20, MAX_M = 31;
 int n, m, k;
 int dx[4] = {-1, 0, 1, 0}; // 상, 우, 하, 좌
 int dy[4] = {0, 1, 0, -1};
-int gun[MAX_N][MAX_N];
 int grid[MAX_N][MAX_N];
 int points[MAX_M];
 
@@ -16,14 +14,13 @@ struct Player{
 };
 
 Player players[MAX_M];
-
 priority_queue<int> pq[MAX_N][MAX_N];
 
 void traverse_gun(){
     cout << "traverse gun.. " << endl;
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            cout << gun[i][j] << ' ';
+            cout  << pq[i][j].top() << ' ';
         }
         cout << endl;
     }
@@ -48,7 +45,6 @@ void traverse_player(){
 }
 
 void init(){
-    memset(gun, 0, sizeof(gun));
     memset(points, 0, sizeof(points));
     memset(grid, 0, sizeof(grid));
 
@@ -64,19 +60,19 @@ void init(){
 }
 
 void input(){
+    int data;
     cin >> n >> m >> k;
 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            cin >> gun[i][j];
-            pq[i][j].push(gun[i][j]);
+            cin >> data;
+            pq[i][j].push(data);
         }
     }
 
     for (int i = 1; i <= m; ++i) {
         cin >> players[i].x >> players[i].y >> players[i].d >> players[i].s;
         players[i].x--, players[i].y--;
-
         grid[players[i].x][players[i].y] = i;
     }
 }
@@ -100,7 +96,6 @@ bool is_grid(int x, int y){
 void phase_1(){
     for(int i = 1; i <= m; ++i){
         // traverse_player();
-
         auto & p = players[i];
         // cout << "player " << i << " pos:  " << p.x << ' ' << p.y <<endl;
 
@@ -114,25 +109,26 @@ void phase_1(){
             nx = p.x + dx[p.d], ny = p.y + dy[p.d];
         }
 
-        p.x = nx, p.y = ny;
+
         // cout << "px, py: " << p.x << ' ' << p.y << endl;
 
         // case 1. 이동한 좌표에 player X
-        if(grid[p.x][p.y] == 0){
+        if(grid[nx][ny] == 0){
             // 이동
-            grid[p.x][p.y] = i;
+            p.x = nx, p.y = ny;
+            grid[nx][ny] = i;
 
             // 해당 칸 총 확인
             // 총 없으면 아무 일 X
-            if(pq[p.x][p.y].top() == 0) continue;
+            if(pq[nx][ny].empty() || pq[nx][ny].top() == 0) continue;
 
             // 플레이어 총이랑 비교 후 더 센총 고르기
-            replace_gun(p.x, p.y);
+            replace_gun(nx, ny);
         }
 
         // case 2. 이동한 좌표에 player O
         else{
-            int idx_enemy = grid[p.x][p.y];
+            int idx_enemy = grid[nx][ny];
             auto & enemy = players[idx_enemy];
 
             int winner, loser, my_p = p.s + p.g, enemy_p = enemy.s + enemy.g;
@@ -163,7 +159,7 @@ void phase_1(){
 
             // 진 플레이어 이동
             for (int j = 0; j < 4; ++j) {
-                int nnx = l.x + dx[l.d], nny = l.y + dy[l.d];
+                int nnx = nx + dx[l.d], nny = ny + dy[l.d];
                 if(!is_grid(nnx, nny) || grid[nnx][nny] != 0){
                     l.d = (l.d + 1) % 4;
                     continue;
@@ -185,7 +181,7 @@ void phase_1(){
 
             w.x = nx, w.y = ny;
 
-            replace_gun(p.x, p.y);
+            replace_gun(w.x, w.y);
         }
     }
 }
@@ -203,7 +199,7 @@ void run(){
 }
 
 int main() {
-    //freopen("input.txt", "r", stdin);
+    // freopen("input.txt", "r", stdin);
     init();
     input();
     run();
