2e43e7eac594853b27be5529906b9d67cab87f00
Seongguk Park <126008521+SeonggukPark@users.noreply.github.com>
2024-10-11 03:45:18 +0900

    Title: 꼬리잡기놀이, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241011/꼬리잡기놀이/tail-catch-play.cpp
+++ 241011/꼬리잡기놀이/tail-catch-play.cpp
@@ -16,7 +16,7 @@ int cur_turn, n, m, k, rst, grid[MAX_N][MAX_N], group[MAX_N][MAX_N];
 bool visited[MAX_N][MAX_N];
 int dx[4] = { 0, -1, 0, 1 }; // right, up, left, down
 int dy[4] = { 1, 0, -1, 0 };
-vector<pii> headers;
+vector<pii> headers, tails;
 queue<pii> q;
 
 void traverse_group() {
@@ -38,7 +38,7 @@ bool is_valid(int nx, int ny) {
 
 void init() {
     int team_idx = 1;
-    headers.push_back({ -1, -1 }); // idx 맞추기 위한 dummy node
+    headers.push_back({ -1, -1 }), tails.push_back({ -1, -1 }); // idx 맞추기 위한 dummy node
 
     cin >> n >> m >> k;
     for (int i = 0; i < n; ++i) {
@@ -46,6 +46,7 @@ void init() {
             cin >> grid[i][j];
             if (grid[i][j] == 0) group[i][j] = -1; // 벽인 경우
             if (grid[i][j] == 1) headers.push_back({ i, j });
+            if (grid[i][j] == 3) tails.push_back({ i, j });
         }
     }
 
@@ -75,38 +76,34 @@ void init() {
 
 void group_move() {
     for (int i = 1; i < (int)headers.size(); ++i) { // 각 그룹 머리 전진
-        pii & cur = headers[i];
+        pii & cur_h = headers[i], & cur_t = tails[i];
+        pii new_head, new_tail;
 
-        // bfs 돌려서 꼬리 찾기
-        for (int i = 0; i < n; ++i) fill(visited[i], visited[i] + n, false);
-        q = {};
-        q.push({ cur.x, cur.y });
-        visited[cur.x][cur.y] = true;
-
-        pii tail;
-        while (!q.empty()) {
-            tail = q.front(); q.pop();
-            group[tail.x][tail.y] = i;
-
-            for (int dir = 0; dir < 4; ++dir) {
-                int nx = tail.x + dx[dir], ny = tail.y + dy[dir];
-                if (!is_valid(nx, ny) || visited[nx][ny] || group[nx][ny] != i) continue;
-                visited[nx][ny] = true;
-                q.push({ nx, ny });
-                break;
-            }
-        }
-
-        // 꼬리 빈 경로로 매우기
-        group[tail.x][tail.y] = 0;
-
-        // 머리 기준 4방면 중 빈 경로로 headers 변경
+        // 새로운 머리 찾기
         for (int dir = 0; dir < 4; ++dir) {
-            int nx = cur.x + dx[dir], ny = cur.y + dy[dir];
-            if (!is_valid(nx, ny) || group[nx][ny] != 0) continue;
-            cur = { nx, ny }; group[nx][ny] = i;
+            int nx = cur_h.x + dx[dir], ny = cur_h.y + dy[dir];
+            if (!is_valid(nx, ny) || group[nx][ny] == -1) continue;
+            if (make_pair(nx, ny) != cur_t && group[nx][ny] == i) continue;
+            new_head = { nx, ny };
             break;
         }
+
+        // 새로운 꼬리 찾기
+        for (int dir = 0; dir < 4; ++dir) {
+            int nx = cur_t.x + dx[dir], ny = cur_t.y + dy[dir];
+            if (!is_valid(nx, ny) || group[nx][ny] != i) continue;
+            if (make_pair(nx, ny) == cur_h) continue; // 머리-꼬리 이어진 경우 대비
+            new_tail = { nx, ny };
+            break;
+        }
+
+        // 현재 꼬리 지우기
+        group[cur_t.x][cur_t.y] = 0;
+        cur_t = new_tail;
+
+        // 새 머리 넣기
+        cur_h = new_head;
+        group[cur_h.x][cur_h.y] = i;
     }
 
     // traverse_group();
@@ -147,13 +144,13 @@ void throw_ball() {
     while (true) {
         if (group[pos.x][pos.y] > 0) {
             int seq_cnt = 0, idx = group[pos.x][pos.y];
+
+            // cout << "hit_info: " << pos.x << ' ' << pos.y << ' ' << idx << endl;
+
             pii top;
             pii & head = headers[idx];
 
-            // cout << "hit info: " << pos.x << ' ' << pos.y << ' ' << idx << endl;
-
-
-            // bfs 돌려서 꼬리 찾기
+            // bfs 돌려서 위치 찾기
             for (int i = 0; i < n; ++i) fill(visited[i], visited[i] + n, false);
             q = {};
             q.push({ head.x, head.y });
@@ -163,7 +160,12 @@ void throw_ball() {
                 top = q.front(); q.pop();
                 seq_cnt++;
 
-                if (pos.x == top.x && pos.y == top.y) rst += seq_cnt * seq_cnt;
+                if (pos.x == top.x && pos.y == top.y) {
+                    rst += seq_cnt * seq_cnt;
+                    swap(headers[idx], tails[idx]);
+                    // cout << "head, tail: " << headers[idx].x << ' ' << headers[idx].y << ' ' << tails[idx].x << ' ' << tails[idx].y << endl;
+                    return;
+                }
 
                 for (int dir = 0; dir < 4; ++dir) {
                     int nx = top.x + dx[dir], ny = top.y + dy[dir];
@@ -173,18 +175,11 @@ void throw_ball() {
                     break;
                 }
             }
-
-            head = top;
-            break;
         }
 
         pos.x += dx[ball_dir], pos.y += dy[ball_dir];
         if (!is_valid(pos.x, pos.y)) break; // 맞은 사람 없이 종료
     }
-
-    
-    // 점수 누적
-    // 맞은 팀 꼬리 찾아서 머리랑 위치 변경
 }
 
 void solve() {
@@ -194,6 +189,7 @@ void solve() {
         cur_turn++;
         group_move();
         throw_ball();
+        // traverse_group();
     }
 
     cout << rst;
@@ -202,7 +198,6 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    // freopen("input.txt", "r", stdin);
     init();
     solve();
     return 0;
